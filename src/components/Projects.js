80620f8f04813bf56374daf752c3f89e2801e187
Tarun Gupta <tarun234.tg@gmail.com>
2024-11-11 14:43:39 +0530

    added new project and some effects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,37 +1,139 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import './Projects.css';
 import { motion } from "framer-motion";
 import { Link } from 'react-router-dom';
-// import data from '../projects/data';
+import img1 from '../assets/concrete2.png';
+import img2 from '../assets/aaargh.png';
+import img3 from '../assets/cognify2.png';
+
+const HoverImage = React.memo(({ src, position }) => (
+  <img
+    src={src}
+    alt=''
+    className='hover-image'
+    style={{
+      top: `${position.y}px`,
+      left: `${position.x}px`,
+      transform: 'translate(-50%, -50%)',
+      position: 'fixed',
+    }}
+  />
+));
 
 const Projects = () => {
+  const [showImage, setShowImage] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [activeImage, setActiveImage] = useState(null);
+
+  const handleMouseEnter = (image) => {
+    setActiveImage(image);
+    setShowImage(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowImage(false);
+    setActiveImage(null);
+  };
+
+  const handleMouseMove = useCallback(
+    (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    },
+    [setPosition]
+  );
+
   return (
     <div className='projectbox'>
+      {showImage && activeImage && (
+        <HoverImage src={activeImage} position={position} />
+      )}
       <div className='project'>
-        <motion.div className='ptitle' initial={{ scale: 0.8 }} whileInView={{ scale: 1 }} transition={{duration:0}}>Concrete Damage Detector</motion.div>
-        <div className='pdesc'>Implemented a Deep Learning model using YOLO-v8 which can detect and segment damages on concrete surfaces in real-time and help maintain strength and integrity of buildings. The model is trained on a custom dataset made using Roboflow.</div>
-        <div className='plink'><a href='https://github.com/MaybeTarun/Concrete-Damage-Detector' target='_blank' rel="noreferrer"><button>Link</button></a></div>
+        <motion.div
+          className='ptitle'
+          initial={{ scale: 0.8 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0 }}
+          onMouseEnter={() => handleMouseEnter(img1)}
+          onMouseLeave={handleMouseLeave}
+          onMouseMove={handleMouseMove}
+        >
+          Concrete Damage Detector
+        </motion.div>
+        <div className='pdesc'>
+          Implemented a Deep Learning model using YOLO-v8 which can detect and segment damages on concrete surfaces in real-time and help maintain strength and integrity of buildings. The model is trained on a custom dataset made using Roboflow.
+        </div>
+        <div className='plink'>
+          <a href='https://github.com/MaybeTarun/Concrete-Damage-Detector' target='_blank' rel="noreferrer">
+            <button>Link</button>
+          </a>
+        </div>
       </div>
       <div className='line2'></div>
       <div className='projectt'>
-        <motion.div className='ptitle' initial={{ scale: 0.8 }} whileInView={{ scale: 1 }} transition={{duration:0}}>AAARGH !</motion.div>
-          <div className='pdesc'>Innovated and crafted a game inspired by Flappy Bird, featuring unique sound-based controls. You would have to scream everytime you want the player to jump.</div>
-          <div className='plink'><a href='https://aaargh.vercel.app/' target='_blank' rel="noreferrer"><button>Link</button></a></div>
+        <motion.div
+          className='ptitle'
+          initial={{ scale: 0.8 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0 }}
+          onMouseEnter={() => handleMouseEnter(img2)}
+          onMouseLeave={handleMouseLeave}
+          onMouseMove={handleMouseMove}
+        >
+          AAARGH !
+        </motion.div>
+        <div className='pdesc'>
+          Innovated and crafted a game inspired by Flappy Bird, featuring unique sound-based controls. You would have to scream every time you want the player to jump.
         </div>
+        <div className='plink'>
+          <a href='https://aaargh.vercel.app/' target='_blank' rel="noreferrer">
+            <button>Link</button>
+          </a>
+        </div>
+      </div>
       <div className='line2'></div>
       <div className='project'>
-        <motion.div className='ptitle' initial={{ scale: 0.8 }} whileInView={{ scale: 1 }} transition={{duration:0}}>Cognify</motion.div>
-          <div className='pdesc'>Developed a cloud based learning platform using Kotlin and XML that automates learning methods making it easy for the user to learn and understand things. It uses OpenAI as its AI model.</div>
-          <div className='plink'><a href='https://maybetarun.github.io/CognifyWebsite.github.io/' target='_blank' rel="noreferrer"><button>Link</button></a></div>
+        <motion.div
+          className='ptitle'
+          initial={{ scale: 0.8 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0 }}
+          onMouseEnter={() => handleMouseEnter(img3)}
+          onMouseLeave={handleMouseLeave}
+          onMouseMove={handleMouseMove}
+        >
+          Cognify
+        </motion.div>
+        <div className='pdesc'>
+          Developed a cloud-based learning platform using Kotlin and XML that automates learning methods, making it easy for the user to learn and understand things. It uses OpenAI as its AI model.
         </div>
+        <div className='plink'>
+          <a href='https://maybetarun.github.io/CognifyWebsite.github.io/' target='_blank' rel="noreferrer">
+            <button>Link</button>
+          </a>
+        </div>
+      </div>
       <div className='line2'></div>
       <div className='projectt'>
-        <motion.div className='ptitle' initial={{ scale: 0.8 }} whileInView={{ scale: 1 }} transition={{duration:0}}>More Projects</motion.div>
-        <div className='pdesc'>You can check out all my projects and ideas by clicking on the button below</div>
-        <div className='plink'><Link to='/projects'><button>Link</button></Link></div>
+        <motion.div
+          className='ptitle'
+          initial={{ scale: 0.8 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0 }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onMouseMove={handleMouseMove}
+        >
+          More Projects
+        </motion.div>
+        <div className='pdesc'>
+          You can check out all my projects and ideas by clicking on the button below.
+        </div>
+        <div className='plink'>
+          <Link to='/projects'><button>Link</button></Link>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Projects;
--- src/projects/data.js
+++ src/projects/data.js
@@ -14,120 +14,128 @@ import dad from "../assets/dad.png";
 import calculator from "../assets/calculator.png";
 import bank from "../assets/bank.png";
 import dice from "../assets/dice.png";
+import surgeon from "../assets/surgeon.png";
 
 const data = [
     {
-      name: `Concrete Damage Detector`,
+      name: `Concrete Damage Detector (Python Project)`,
       desc: `Implemented a Deep Learning model using YOLO-v8 which can detect and segment damages on concrete surfaces in real-time and help maintain strength and integrity of buildings. The model is trained on a custom dataset made using Roboflow.`,
       link: `https://github.com/MaybeTarun/Concrete-Damage-Detector`,
       skills: [`Python`, `YoloV8`],
       image: concrete
     },
     {
-      name: `Aaargh!`,
+      name: `Aaargh! (Web Game)`,
       desc: `Game inspired by Flappy Bird. The player is controlled by making weird noises like "aaaah" or "aargh".`,
       link: `https://www.maybetarun.in/projects/aaargh`,
       skills: [`React`],
       image: aaargh
     },
     {
-      name: `Cognify`,
+      name: `Cognify (Android App)`,
       desc: `Developed a cloud based learning platform using Kotlin and XML that automates learning methods making it easy for the user to learn and understand things. It uses OpenAI as its AI model.`,
       link: `https://github.com/MaybeTarun/Cognify`,
       skills: [`Kotlin`, `XML`],
       image: cognify
     },
     {
-      name: `Foresty`,
+      name: `Foresty (Web App)`,
       desc: `Created a user-friendly platform that helps users in finding vulnerabilities (such as open ports) in their websites which anyone could have used to hack them. It uses a shell script which automates the process of vulnerability assessment.`,
       link: `https://github.com/MaybeTarun/Foresty`,
       skills: [`HTML`, `CSS`, `Javascript`, `Shell`],
       image: foresty
     },
     {
-      name: `Roshni Sinks`,
+      name: `Doctor Portfolio (Freelance Project)`,
+      desc: `Designed and Developed a Responsive Portfolio Website for a Doctor as part of my freelance career.`,
+      link: `https://www.surgeonrkl.com`,
+      skills: [`React`, `TailwindCSS`],
+      image: surgeon
+    },
+    {
+      name: `Roshni Sinks (Freelance Project)`,
       desc: `An E-Commerce platform for a manufacturing business "Roshni". Still under development`,
       link: `https://roshnisinks.vercel.app/`,
       skills: [`React`],
       image: roshni
     },
     {
-      name: `Finance Tracker`,
+      name: `Finance Tracker (Web App)`,
       desc: `An MERN stack application for tracking personal finances. Uses Clerk for user management`,
       link: `https://financetracker-neon.vercel.app/`,
       skills: [`React`, `MongoDB`],
       image: finance
     },
     {
-      name: `Know-About`,
+      name: `Know-About (NPM Package)`,
       desc: `Built an NPM Package that gives you info about people when you run the command "npx know-about [name]". Currently consists of only 2-3 people's info but I plan to add more, so if you want to add yours too just dm me on twitter ;)`,
       link: `https://www.npmjs.com/package/know-about`,
       skills: [`Node`],
       image: knowabout
     },
     {
-      name: `Image Colorizer`,
+      name: `Image Colorizer (Web App)`,
       desc: `This website is a photo colorizer. You can upload your black and white photos and it uses a machine learning model, created by Rich Zhang, to colorize the images for you!`,
       link: `https://github.com/MaybeTarun/ImageColorizer`,
       skills: [`Flask`, `HTML`],
       image: colorizer
     },
     {
-      name: `Holostry`,
+      name: `Holostry (Android App)`,
       desc: `Developed this project for a 24hr Hackathon - MOZOHACK with my friends. It consists of 3D holographic model of carbon compounds which students can use to visualize the compounds in 3 dimensions making it easier for them to understand.`,
       link: `https://github.com/MaybeTarun/Holostry`,
       skills: [`Dart`, `Blender`],
       image: holostry
     },
     {
-      name: `Payback-to-ya`,
+      name: `Payback-to-ya (NPM Package)`,
       desc: `Created an NPM package which you can add to your website if your client doesn't pay you on time. It will slowly make the website disappear in 7 days and the client will be in ShIk sHaK ShOk.`,
       link: `https://www.npmjs.com/package/payback-to-ya`,
       skills: [`Node`],
       image: payback
     },
     {
-      name: `Sentiment Analyzer`,
+      name: `Sentiment Analyzer (Python Project)`,
       desc: `It is a Python application that utilizes the Boto3 library to interact with Amazon Comprehend's sentiment analysis service. The application is built with a graphical user interface (GUI) created using the Tkinter library.`,
       link: `https://github.com/MaybeTarun/SentimentAnalyzer`,
       skills: [`Boto3`, `Tkinter`, `AWS Comprehend`],
       image: sentiment
     },
     {
-      name: `Spree`,
+      name: `Spree (Web App)`,
       desc: `Just a movie streaming website that uses OMDb API.`,
       link: `https://github.com/MaybeTarun/Spree`,
       skills: [`HTML`, `CSS`, `Javascript`],
       image: spree
     },
     {
-      name: `Dad Jokes`,
+      name: `Dad Jokes (Chrome Extension)`,
       desc: `Built a chrome extension that displays random dad jokes whenever used. It uses an API called icanhazdadjoke. You can check it out by turning on developer mode in chrome extensions and loading my extension.`,
       link: `https://github.com/MaybeTarun/DadJokes-ChromeExt`,
       skills: [`HTML`, `CSS`, `Javascript`],
       image: dad
     },
     {
-      name: `Aint a Calculator`,
+      name: `Aint a Calculator (Web App)`,
       desc: `I honestly don't know why I built this. But whats done is done. It is a calculator which only works when you switch its lever on (you have to solve a random math question in order to switch it on). It also displays some math puns (coz why not).`,
       link: `https://maybetarun.github.io/AintACalculator.github.io/`,
       skills: [`HTML`, `CSS`, `Javascript`],
       image: calculator
     },
     {
-      name: `Bank Management System`,
+      name: `Bank Management System (Python Project)`,
       desc: `Built a very basic bank management system using python and mysql for a school project. It's one of my first school projects.`,
       link: `https://github.com/MaybeTarun/Bank-Management-Project`,
       skills: [`Numpy`, `Pandas`, `MySQL`],
       image: bank
     },
     {
-      name: `Dice Simulator`,
+      name: `Dice Simulator (Python Project)`,
       desc: `A very normal dice simulator built with Python. Also one of my very old school project.`,
       link: `https://github.com/MaybeTarun/Dice-Simulator`,
       skills: [`Tkinter`],
       image: dice
-    }
+    },
   ];
   
   export default data;
